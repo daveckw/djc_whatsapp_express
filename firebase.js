af9ca320e13c4b56b405288bd3dac1cb0ee03642
daveckw <daveckw@gmail.com>
2023-06-17 17:52:38 +0800

    Fix path

--- firebase.js
+++ firebase.js
@@ -1,10 +1,6 @@
 const admin = require("firebase-admin");
-const path = require("path");
-const serviceAccount = require(path.join(
-    ".",
-    "private",
-    "facebook-api-59e5c-firebase-adminsdk-uqibe-c3ed28858c.json"
-));
+
+const serviceAccount = require("./private/facebook-api-59e5c-firebase-adminsdk-uqibe-c3ed28858c.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
