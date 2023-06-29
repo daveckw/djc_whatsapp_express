9001b161a5d97f934d28d881c635c80798a14283
daveckw <daveckw@gmail.com>
2023-06-29 08:19:49 +0800

    Add error handling

--- utils-functions/determineDestinationInstance.js
+++ utils-functions/determineDestinationInstance.js
@@ -2,29 +2,34 @@ const { compute, auth } = require("../compute");
 const { firestore } = require("../firebase");
 
 async function determineDestinationInstance(clientId) {
-    // Get the instance name from Firestore.
-    const doc = await firestore
-        .collection("whatsappClients")
-        .doc(clientId)
-        .get();
-    if (!doc.exists) {
-        console.log(`No document for clientId: ${clientId}`);
-        return "";
+    try {
+        // Get the instance name from Firestore.
+        const doc = await firestore
+            .collection("whatsappClients")
+            .doc(clientId)
+            .get();
+        if (!doc.exists) {
+            console.log(`No document for clientId: ${clientId}`);
+            return "";
+        }
+        const instanceName = doc.data().instanceName;
+
+        // Get the instance details from Compute Engine.
+        const instance = await compute.instances.get({
+            project: "djc-whatsapp", // Replace with your project ID.
+            zone: "asia-southeast1-b", // Replace with your zone.
+            instance: instanceName,
+            auth: auth
+        });
+
+        // Get the IP address from the instance details.
+        const ipAddress =
+            instance.data.networkInterfaces[0].accessConfigs[0].natIP;
+
+        return ipAddress;
+    } catch (error) {
+        console.log(error);
     }
-    const instanceName = doc.data().instanceName;
-
-    // Get the instance details from Compute Engine.
-    const instance = await compute.instances.get({
-        project: "djc-whatsapp", // Replace with your project ID.
-        zone: "asia-southeast1-b", // Replace with your zone.
-        instance: instanceName,
-        auth: auth
-    });
-
-    // Get the IP address from the instance details.
-    const ipAddress = instance.data.networkInterfaces[0].accessConfigs[0].natIP;
-
-    return ipAddress;
 }
 
 exports.determineDestinationInstance = determineDestinationInstance;
